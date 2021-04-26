b95a1183776623479c9d4d3c2906954e466005f6
Al Nasimul Haque <alnasimul@gmail.com>
2021-04-27 01:08:27 +0600

    simple dynamic api call learned

--- index.js
+++ index.js
@@ -6,4 +6,22 @@ app.get('/',(req,res) => {
     res.send('I know how to open node....YAY');
 })
 
+app.get('/fruits/banana', (req,res) => {
+    res.send({
+        name:"Banana",
+        quantity:1000,
+        price:10000
+
+    })
+})
+
+const users = ['Sadaf','Orni','Rafa','Karim','Susmita','Nayem'];
+
+app.get('/users/:id', (req,res) => {
+    const id = req.params.id;
+    const name = users[id];
+
+    res.send({name,id});
+})
+
 app.listen(3000,() => console.log('Listening to port 3000'));
